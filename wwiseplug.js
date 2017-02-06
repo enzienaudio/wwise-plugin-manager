63b56c0205f4025c56c44dfeae7d75b68153e024
Joe White <white.joe4@gmail.com>
2017-02-06 15:34:39 -0800

    sort wwise packages by aplhabetical order

--- wwiseplug.js
+++ wwiseplug.js
@@ -1,6 +1,6 @@
-var jetpack = require("fs-jetpack")
-var jquery = require("jquery")
-var path = require("path")
+const jetpack = require("fs-jetpack")
+const jquery = require("jquery")
+const path = require("path")
 
 String.format = function() {
   var s = arguments[0]
@@ -43,6 +43,24 @@ wwiseplug.add_plugin = function(plugin_path, wwise_version) {
   } else {
     console.error(String.format("{0} not a valid Wwise plugin", plugin_path))
   }
+
+  // Note:(joe) this apparently should be done but doesn't seem to work,
+  // or actually make a difference keeping around in case we need to come back to it.
+  // exec(
+  //   "./bin/wineprefixcreate --snapshot", {
+  //     cwd: "/Applications/Audiokinetic/Wwise\\ 2016.1.0.5775/Wwise.app/Contents/SharedSupport/Wwise2016",
+  //     env: {
+  //       CX_ROOT: "/Applications/Audiokinetic/Wwise\\ 2016.1.0.5775/Wwise.app/Contents/SharedSupport/Wwise2016/",
+  //       WINEPREFIX: "/Applications/Audiokinetic/Wwise\\ 2016.1.0.5775/Wwise.app/Contents/SharedSupport/Wwise2016/support/wwise/"
+  //     }
+  //   }, function(error, stdout, stderr) {
+  //     console.log('stdout: ', stdout);
+  //     console.log('stderr: ', stderr);
+  //     if (error !== null) {
+  //       console.log('exec error: ', error);
+  //     }
+  // })
+
   this.refresh()
 }
 
@@ -55,81 +73,83 @@ wwiseplug.remove_plugin = function(plugin_name, wwise_version) {
 
     jetpack.remove(path.format({ dir: plug_dir, name: plug_name, ext: ".xml"}))
     jetpack.remove(path.format({ dir: plug_dir, name: plug_name, ext: ".dll"}))
-
   }
   this.refresh()
 }
 
 wwiseplug.refresh = function() {
   let container = $("#wwise")
-  container.html("")
+  container.html("") // clear container
 
-  // loop over all wwise application directories
-  $.each(jetpack.list(this.get_app_dir()), function (i, dirName) {
+  // generate list of all wwise application directories
+  let wwise_apps = jetpack.list(this.get_app_dir()).reverse().filter(function (x) {
+    return (~x.indexOf("Wwise"))
+  })
 
-    if (~dirName.indexOf("Wwise")) {
-      // add wwise version header
-      let row = $("<div/>", {class: "row", id: dirName})
-      row.append($(String.format("<div class=\"col\"><h3>{0}</h3><hr class=\"wwiseheader\"/></div>", dirName)))
+  $.each(wwise_apps, function(i, dirName) {
 
-      let drop_area = $("<div/>", {class:"drop-area", id: "droparea-" + dirName})
-      // add drag and drop event handlers
-      drop_area.bind("drop", (event) => {
-        wwiseplug.add_plugin(
-            event.originalEvent.dataTransfer.files[0].path, // plugin path
-            event.target.id.split("-")[1]) // wwise version
-        event.preventDefault()
-      })
-      drop_area.bind("dragover", (event) => { event.preventDefault() })
-      drop_area.bind("dragenter", (event) => { event.target.style.border = "2px dotted #d1e5d5" })
-      drop_area.bind("dragleave", (event) => { event.target.style.border = "1px solid #fafafa" })
+    // add wwise version header
+    let row = $("<div/>", {class: "row", id: dirName})
+    row.append($(String.format("<div class=\"col\"><h3>{0}</h3><hr class=\"wwiseheader\"/></div>", dirName)))
 
-      let drop_text = $("<div/>", {class:"drop-text", text:"Drop Wwise *.xml or *.dll plugin files here to install."})
-      drop_area.append(drop_text)
-      row.append(drop_area)
+    let drop_area = $("<div/>", {class:"drop-area", id: "droparea-" + dirName})
+    // add drag and drop event handlers
+    drop_area.bind("drop", (event) => {
+      wwiseplug.add_plugin(
+          event.originalEvent.dataTransfer.files[0].path, // plugin path
+          event.target.id.split("-")[1]) // wwise version
+      event.preventDefault()
+    })
+    drop_area.bind("dragover", (event) => { event.preventDefault() })
+    drop_area.bind("dragenter", (event) => { event.target.style.border = "2px dotted #d1e5d5" })
+    drop_area.bind("dragleave", (event) => { event.target.style.border = "1px solid #fafafa" })
 
-      // construct plugin table
-      let table = $("<table/>", {class: "table table-hover"})
+    let drop_text = $("<div/>", {class:"drop-text", text:"Drop Wwise *.xml or *.dll plugin files here to install."})
+    drop_area.append(drop_text)
+    row.append(drop_area)
+
+    // construct plugin table
+    let table = $("<table/>", {class: "table table-hover"})
+    let table_tr = $("<tr/>")
+    table_tr.append($("<th>Plugin Name</th>"))
+    table_tr.append($("<th>Type</th>"))
+    table_tr.append($("<th>Delete</th>"))
+
+    let table_head = $("<thead/>")
+    table_head.append(table_tr)
+    table.append(table_head)
+
+    let table_body = $("<tbody/>")
+
+    // generate list of installed heavy plugins
+    plugins = jetpack.list(wwiseplug.get_plugin_dir(dirName)).filter(function (x) {
+      return (~x.indexOf("Hv_") && path.extname(x) === ".dll")
+    })
+
+    $.each(plugins, function(i, pluginName) {
+
       let table_tr = $("<tr/>")
-      table_tr.append($("<th>Plugin Name</th>"))
-      table_tr.append($("<th>Type</th>"))
-      table_tr.append($("<th>Delete</th>"))
+      table_tr.append($(String.format("<td><a>{0}</a></td>", pluginName.split("_")[1])))
+      table_tr.append($(String.format("<td>{0}</td>", (~pluginName.indexOf("Source")) ? "Source" : "FX")))
 
-      let table_head = $("<thead/>")
-      table_head.append(table_tr)
-      table.append(table_head)
-
-      let table_body = $("<tbody/>")
-
-      // loop over all installed plugins
-      $.each(jetpack.list(wwiseplug.get_plugin_dir(dirName)), function (i, pluginName) {
-
-        if (~pluginName.indexOf("Hv_") && path.extname(pluginName) === ".dll") {
-          // create plugin list
-          let table_tr = $("<tr/>")
-          table_tr.append($(String.format("<td><a>{0}</a></td>", pluginName.split("_")[1])))
-          table_tr.append($(String.format("<td>{0}</td>", (~pluginName.indexOf("Source")) ? "Source" : "FX")))
-
-          let delete_link = $("<a/>")
-          delete_link.append($("<i/>", {class:"glyphicon glyphicon-trash"}))
-          delete_link.bind("click", (event) => {
-            wwiseplug.remove_plugin(pluginName, dirName)
-          })
-          table_tr.append($("<td/>").append(delete_link))
-          table_body.append(table_tr)
-
-          // TODO:(joe) check if xml is installed
-        }
+      let delete_link = $("<a/>")
+      delete_link.append($("<i/>", {class:"glyphicon glyphicon-trash"}))
+      delete_link.bind("click", (event) => {
+        wwiseplug.remove_plugin(pluginName, dirName)
       })
+      table_tr.append($("<td/>").append(delete_link))
+      table_body.append(table_tr)
 
-      // don't display plugin table if it's empty
-      if (table_body.html() !== "") {
-        table.append(table_body)
-        row.append(table)
-      }
+      // TODO:(joe) check if xml is installed
+    })
 
-      // add to the main body
-      container.append(row)
+    // don't display plugin table if it's empty
+    if (table_body.html() !== "") {
+      table.append(table_body)
+      row.append(table)
     }
+
+    // add to the main body
+    container.append(row)
   })
 }
